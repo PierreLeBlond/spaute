bb90a5dcb6de5c8cc220660623e48529a530b6e8
Lespingal <pierre.lespingal@gmail.com>
2023-03-13 16:22:53 +0100

    feat: Add player creation form

--- src/routes/band/[bandId]/players/+page.server.ts
+++ src/routes/band/[bandId]/players/+page.server.ts
@@ -1,9 +1,28 @@
 import prisma from '$lib/prisma'
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
   const players = await prisma.player.findMany();
   return {
     players
+  }
+}
+
+export const actions: Actions = {
+  default: async ({ params, request }) => {
+    const { bandId } = params;
+    const data = await request.formData();
+    const name = data.get("name") as string;
+    const response = await prisma.player.create({
+      data: {
+        band: {
+          connect: {
+            id: Number(bandId)
+          }
+        },
+        name
+      }
+    });
+    return { success: true, response };
   }
 }
--- src/routes/band/[bandId]/players/+page.svelte
+++ src/routes/band/[bandId]/players/+page.svelte
@@ -6,7 +6,7 @@
   $: players = data.players;
 </script>
 
-<ul class="grid grid-cols-1 gap-y-2">
+<ul class="grid grid-cols-1 gap-y-2 pb-2">
   {#each players as player}
     <li class="w-64">
       <a
@@ -18,3 +18,15 @@
     </li>
   {/each}
 </ul>
+<div
+  class="w-64 p-2 border border-yellow-200 rounded text-sm grid grid-cols-1 gap-y-1"
+>
+  <p class="text-center">Add a player</p>
+  <form class="grid grid-cols-2 gap-y-2" method="POST">
+    <label for="name-input"> name </label>
+    <input type="text" class="bg-zinc-600" name="name" id="name-input" />
+    <button class="col-span-2 w-full p-2 border rounded border-yellow-600"
+      >Add</button
+    >
+  </form>
+</div>
