edeb56e9ea8cf7a7120ac3490e3c260bb824b734
Lespingal <pierre.lespingal@gmail.com>
2023-03-15 17:03:29 +0100

    fix: Fix hidden instruments route

--- src/routes/(app)/instruments/+page.svelte
+++ src/routes/(app)/instruments/+page.svelte
@@ -3,28 +3,32 @@
 
   export let data: PageData;
 
-  let name: string = "";
-  const add = async () => {
-    const response = await fetch("./instruments", {
-      method: "POST",
-      body: JSON.stringify({ data: { name } }),
-      headers: {
-        "content-type": "application/json",
-      },
-    });
-    const instrument = await response.json();
-    data.instruments = [...data.instruments, instrument];
-    name = "";
-  };
+  $: instruments = data.instruments;
 </script>
 
-<h1>Instruments :</h1>
-
-{#each data.instruments as instrument}
-  <h2>
-    {instrument.name}
-  </h2>
-{/each}
-
-<input type="text" bind:value={name} />
-<button on:click={add}>add</button>
+<ul
+  class="p-2 w-full sm:w-96 grid grid-cols-1 gap-y-2 mb-2 overflow-y-auto border grow border-yellow-600 rounded"
+>
+  {#each instruments as instrument}
+    <li class="w-full">
+      <p
+        class="p-2 w-full border border-yellow-200 rounded text-sm grid grid-cols-1 gap-y-1"
+      >
+        {instrument.name}
+      </p>
+    </li>
+  {/each}
+</ul>
+<div class="bg-yellow-600 text-zinc-900 h-32 p-2 rounded w-full sm:w-96">
+  <h2 class="w-full text-sm">Add an instrument</h2>
+  <form class="pt-2 grid grid-cols-2 gap-y-2" method="POST">
+    <label for="name-input">name -></label>
+    <input
+      id="name-input"
+      name="name"
+      class="bg-zinc-900 text-yellow-600 rounded"
+      type="text"
+    />
+    <button class="col-span-2 border rounded border-zinc-900">Add</button>
+  </form>
+</div>
