6a597df5a35446ecc623d8ed9ce9fcb11a407a80
Lespingal <pierre.lespingal@gmail.com>
2023-05-03 10:47:52 +0200

    feat: Remove instruments client routes

--- src/lib/trpc/routes/instruments.ts
+++ src/lib/trpc/routes/instruments.ts
@@ -1,7 +1,4 @@
-import { InstrumentCreateInputSchema, InstrumentWhereUniqueInputSchema } from "$lib/generated/zod";
 import prisma from "$lib/prisma";
-import { Prisma } from "@prisma/client";
-import { TRPCError } from "@trpc/server";
 import { t } from "../t";
 
 export const instruments = t.router({
@@ -13,41 +10,5 @@ export const instruments = t.router({
     });
 
     return instruments;
-  }),
-  delete: t.procedure.input(InstrumentWhereUniqueInputSchema).mutation(async ({ input }) => {
-    const response = await prisma.instrument.delete({
-      where: input
-    });
-
-    return response;
-  }),
-  create: t.procedure.input(
-    InstrumentCreateInputSchema
-  ).mutation(async ({ input: { name } }) => {
-    try {
-      const response = await prisma.instrument.create({
-        data: {
-          name
-        }
-      });
-      return {
-        id: response.id,
-        result: response
-      }
-    } catch (error) {
-      if (error instanceof Prisma.PrismaClientKnownRequestError && error.code == 'P2002') {
-        throw new TRPCError({
-          code: 'BAD_REQUEST',
-          message: 'Nom déjà utilisé...',
-          cause: error
-        });
-      }
-
-      throw new TRPCError({
-        code: 'INTERNAL_SERVER_ERROR',
-        message: 'Unexpected prisma error occured.',
-        cause: error
-      });
-    }
   })
 })
